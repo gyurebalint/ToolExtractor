4881a9b4ec243108cb9071a5ac097e2c74f5f4d2
gyurebalint@gmail.com <gyurebalint@gmail.com>
2020-07-12 14:03:16 +0200

    Add all my files

---
+++ ToolExtractor/Extractor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace ToolExtractor
+{
+    class Extractor
+    {
+        static public string UserName = Environment.UserName;
+        static public string NameSpace = "ToolExtractor";
+        static public string DesignData = "DesignData";
+        static public string InventorAddins = "InventorAddins";
+        static public string Templates = "Templates";
+        static public string DesignAcceleratorModels = "DesignAcceleratorModels";
+
+        static public string DesignDataDestinationFolderPath = @"C:\Users\Public\Documents\Autodesk\Inventor 2018\Design Data";
+        static public string InventorAddinsDestinationFolderPath = $@"C:\Users\{UserName}\AppData\Roaming\Autodesk\ApplicationPlugins\busbarTools";
+        static public string TemplatesDestinationFolderPath = @"C:\Users\Public\Documents\Autodesk\Inventor 2018\Templates";
+        static public string DesignAcceleratorModelsDestinationFolderPath = @"C:\Program Files\Autodesk\Inventor 2018\Design Accelerator\models";
+
+        public static Assembly CurrentAssembly = Assembly.GetCallingAssembly();
+        public static void Extract( string outDirectory, string internalFilePath, string resourceName)
+        {
+            using (Stream s = CurrentAssembly.GetManifestResourceStream(NameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName))
+            {
+                using(BinaryReader r = new BinaryReader(s))
+                {
+                    using (FileStream fs = new FileStream(outDirectory + "\\" + resourceName, FileMode.OpenOrCreate))
+                    {
+                        using(BinaryWriter w = new BinaryWriter(fs))
+                        {
+                            w.Write(r.ReadBytes((int)s.Length));
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ ToolExtractor/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ToolExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine( Extractor.CurrentAssembly.Location);
+            try
+            {
+                foreach (string resource in Extractor.CurrentAssembly.GetManifestResourceNames())
+                {
+                    
+                    string[] resourceName = resource.Split('.');
+                    string resourceNameWithExtension = "";
+                    
+                    if (!resource.Contains("addin"))
+                    {
+                         resourceNameWithExtension = resourceName[resourceName.Length - 2] + "." + resourceName[resourceName.Length - 1];
+
+                    }
+                    else
+                    {
+                        resourceNameWithExtension = resourceName[resourceName.Length - 4] + "." + resourceName[resourceName.Length - 3] + "."+ resourceName[resourceName.Length - 2] + "." + resourceName[resourceName.Length - 1];
+                        Console.WriteLine(resource);
+                    }
+                    Console.WriteLine(resourceNameWithExtension); 
+
+
+                    if (resource.Contains(Extractor.DesignData))
+                    {
+                        Extractor.Extract(Extractor.DesignDataDestinationFolderPath, Extractor.DesignData, resourceNameWithExtension);
+                    }
+                    if (resource.Contains(Extractor.InventorAddins))
+                    {
+                        Extractor.Extract(Extractor.InventorAddinsDestinationFolderPath, Extractor.InventorAddins, resourceNameWithExtension);
+                    }
+                    if (resource.Contains(Extractor.Templates))
+                    {
+                        Extractor.Extract(Extractor.TemplatesDestinationFolderPath, Extractor.Templates, resourceNameWithExtension);
+                    }
+                    if (resource.Contains(Extractor.DesignAcceleratorModels))
+                    {
+                        Extractor.Extract(Extractor.DesignAcceleratorModelsDestinationFolderPath, Extractor.DesignAcceleratorModels, resourceNameWithExtension);
+                    }
+
+                }
+                MessageBox.Show("Copying to the Inventor destination folders were SUCCESSFUL!");
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message} \n Copying was unsuccessful! \n Make the files you want to copy as an 'Embedded Resource' ! \n " +
+                    "You can do this by clicking on the file, right bottom side of the screen in the properties window, change the BUILD ACTION to " +
+                    "'Embedded Resource', then build the project again");
+
+                /*"Copying was unsuccessful! \n Make the files you want to copy as an 'Embedded Resource' ! \n " +
+                    "You can do this by clicking on the file, right bottom side of the screen in the properties window, change the BUILD ACTION to " +
+                    "'Embedded Resource', then build the project again");*/
+            }
+            //Console.WriteLine();
+ 
+
+            //Console.ReadLine();
+        }
+    }
+}
+//BusbarTools
+//C:\Users\Gyure Balint\AppData\Roaming\Autodesk\ApplicationPlugins\busbarTools
+
+//Templates
+//C:\Users\Public\Documents\Autodesk\Inventor 2018\Templates
+
+//DesignData
+//C:\Users\Public\Documents\Autodesk\Inventor 2018\Design Data
+
+//Design Accelerator
+//C:\Program Files\Autodesk\Inventor 2018\Design Accelerator\models
+//C:\Program Files\Autodesk\Inventor 2018\Design Accelerator\defaults
